986ba8bf2cd74e463948c77d868fd5a52111d31a
Stanislaw Blicharski <mainly.agn@gmail.com>
2025-06-04 14:24:18 +0200

    google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -116,6 +116,19 @@ export default function RootLayout({
         <meta name="theme-color" content="#e3f0ff" />
         <link rel="canonical" href="https://czeskieprawojazdy.pl/" />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: faqJsonLd }} />
+        
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TL9WPJ9X');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-17128297535"
           strategy="afterInteractive"
@@ -143,6 +156,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-blue-50 text-blue-900`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TL9WPJ9X"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         {children}
         <Footer />
         <Analytics />
